14d4ba82e3bb06fbb9f98bf4153288fea4fd35c4
Bruno Gomes <bruno.gomes.771@outlook.com>
2020-10-19 03:04:30 +0100

    Unity Project

---
+++ PestilenceAway/Assets/Scripts/GamePause.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamePause : MonoBehaviour
+{
+    public static bool gamePaused;
+    public GameObject pauseMenu; //Define in Inspector
+    public GameObject gameUI; //Define in Inspector
+
+    private void Start()
+    {
+        gamePaused = false;
+        pauseMenu.SetActive(false);
+        gameUI.SetActive(true);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        gameUI.SetActive(true);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        gameUI.SetActive(false);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    public void MenuButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
